6b4dbb821e505a3ab86bea050a3019a357027e2f
Vsetis <ondravseta@email.cz>
2023-03-19 20:45:42 +0100

    add system theme only

--- src/components/ChangeTheme.tsx
+++ src/components/ChangeTheme.tsx
@@ -4,23 +4,10 @@ import { CiDark, CiLight } from "react-icons/ci";
 
 export default function ChangeTheme() {
   const { theme, setTheme } = useTheme();
-  const [dark, setDark] = useState(theme === " dark" ? true : false);
-
+  setTheme("system");
   return (
     <>
-      <button
-        className="absolute md:top-0  md:right-[-16px] bottom-0 right-0"
-        onClick={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
-          setDark(!dark);
-        }}
-      >
-        {dark ? (
-          <CiDark className="w-8 h-8" />
-        ) : (
-          <CiLight className="w-8 h-8" />
-        )}
-      </button>
+      <div> {theme}</div>
     </>
   );
 }
