eed66d779a312a1bd0d248c5e388b99d1a9f63ff
Vsetis <ondravseta@email.cz>
2023-04-07 03:01:37 +0200

    update ig url

--- src/components/Menu/Socials.tsx
+++ src/components/Menu/Socials.tsx
@@ -4,7 +4,7 @@ export default function Socials() {
   const socials = [
     { target: "https://github.com/Vsetis", icon: AiFillGithub, id: 1 },
     {
-      target: "https://www.instagram.com/ondra_vseta/",
+      target: "https://www.instagram.com/modernweb.cz/",
       icon: AiFillInstagram,
       id: 2,
     },
@@ -17,7 +17,8 @@ export default function Socials() {
             key={social.id}
             href={social.target}
             className="mr-8"
-            target="_blank">
+            target="_blank"
+          >
             <social.icon className="w-8 h-8" />
           </Link>
         ))}
