d29f4b5719c4f98564e84819b14e454c7fccbf03
Vsetis <ondravseta@email.cz>
2023-04-16 19:23:57 +0200

    fix skills - remove %

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,13 +2,14 @@ import { DiReact } from "react-icons/di";
 import { SiTailwindcss } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { AiFillHtml5 } from "react-icons/ai";
+import { TbBrandNextjs } from "react-icons/tb";
 import { motion } from "framer-motion";
 export default function About() {
   const skills = [
-    { title: " ReactJS 60%", icon: DiReact, id: 1 },
-    { title: " TailwindCSS 90%", icon: SiTailwindcss, id: 2 },
-    { title: " HTML 90%", icon: AiFillHtml5, id: 3 },
-    { title: " MySQL 80%", icon: GrMysql, id: 4 },
+    { title: " NextJs", icon: TbBrandNextjs, id: 1 },
+    { title: " TailwindCSS", icon: SiTailwindcss, id: 2 },
+    { title: " HTML", icon: AiFillHtml5, id: 3 },
+    { title: " MySQL", icon: GrMysql, id: 4 },
   ];
   return (
     <>
