2200dd4e299c2e6125d648a459d1cc5dea84811e
Vsetis <ondravseta@email.cz>
2023-05-31 01:28:13 +0200

    fix syntax

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,63 +1,66 @@
-import Link from "next/link";
-import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
-import { MdEmail, MdPhoneIphone } from "react-icons/md";
-import { BiArrowFromBottom } from "react-icons/bi";
+import Link from 'next/link';
+import { AiFillGithub, AiFillInstagram } from 'react-icons/ai';
+import { MdEmail, MdPhoneIphone } from 'react-icons/md';
+import { BiArrowFromBottom } from 'react-icons/bi';
 export default function Contact() {
-  const links = [{ name: "", target: "#" }];
-  const socials = [
-    { target: "https://github.com/Vsetis", icon: AiFillGithub, id: 1 },
-    {
-      target: "https://www.instagram.com/ondra_vseta/",
-      icon: AiFillInstagram,
-      id: 2,
-    },
-  ];
-  function scroll() {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  }
-  return (
-    <>
-      <div
-        id="Contact"
-        className="dark:bg-black border-t border-black/20 dark:border-white/20 pt-[42px] pb-[16px] px-8  w-full"
-      >
-        <div className="container mx-auto flex md:flex-row flex-col justify-between">
-          <div className="flex flex-col mb-8 md:mb-0">
-            <h2 className="text-xl font-bold mb-3.5">Kontakt</h2>
-            <Link
-              className="flex items-center mb-2"
-              href="mailto:ondravseta@email.cz"
+    const links = [{ name: '', target: '#' }];
+    const socials = [
+        { target: 'https://github.com/Vsetis', icon: AiFillGithub, id: 1 },
+        {
+            target: 'https://www.instagram.com/ondra_vseta/',
+            icon: AiFillInstagram,
+            id: 2,
+        },
+    ];
+    function scroll() {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        });
+    }
+    return (
+        <>
+            <div
+                id="Contact"
+                className="dark:bg-black border-t border-black/20 dark:border-white/20 pt-[42px] pb-[16px] px-8  w-full"
             >
-              <MdEmail className="h-5 w-5 mr-2" />
-              ondravseta@email.cz
-            </Link>
-            <Link className="flex items-center" href="tel:+420 736 137 654">
-              <MdPhoneIphone className="h-5 w-5 mr-2" />
-              +420 736 137 654
-            </Link>
-          </div>
-          <div className="flex flex-row">
-            <button onClick={scroll}>
-              <BiArrowFromBottom className="dark:bg-white/60 bg-black/80 text-white dark:text-black hover:bg-black dark:hover:bg-white/80 transition-all duration-300  w-12 h-12 rounded-full p-2" />
-            </button>
-          </div>
-          <div className="flex flex-col  justify-between">
-            <div className="flex flex-row md:self-end">
-              {socials.map((social) => (
-                <Link key={social.id} href={social.target}>
-                  <social.icon className="w-8 h-8 md:ml-2 md:mt-0  mt-8 mr-5 md:mr-0" />
-                </Link>
-              ))}
+                <div className="container mx-auto flex md:flex-row flex-col justify-between">
+                    <div className="flex flex-col mb-8 md:mb-0">
+                        <h2 className="text-xl font-bold mb-3.5">Kontakt</h2>
+                        <Link
+                            className="flex items-center mb-2"
+                            href="mailto:ondravseta@email.cz"
+                        >
+                            <MdEmail className="h-5 w-5 mr-2" />
+                            ondravseta@email.cz
+                        </Link>
+                        <Link
+                            className="flex items-center"
+                            href="tel:+420 736 137 654"
+                        >
+                            <MdPhoneIphone className="h-5 w-5 mr-2" />
+                            +420 736 137 654
+                        </Link>
+                    </div>
+                    <div className="flex flex-row">
+                        <button onClick={scroll}>
+                            <BiArrowFromBottom className="dark:bg-white/60 bg-black/80 text-white dark:text-black hover:bg-black dark:hover:bg-white/80 transition-all duration-300  w-12 h-12 rounded-full p-2" />
+                        </button>
+                    </div>
+                    <div className="flex flex-col  justify-between">
+                        <div className="flex flex-row md:self-end">
+                            {socials.map((social) => (
+                                <Link key={social.id} href={social.target}>
+                                    <social.icon className="w-8 h-8 md:ml-2 md:mt-0  mt-8 mr-5 md:mr-0" />
+                                </Link>
+                            ))}
+                        </div>
+                        <div className="w-full h-[2px] md:my-8 my-4 bg-black dark:bg-white" />
+                        <h2>© 2023 Ondřej Všetička</h2>
+                    </div>
+                </div>
             </div>
-            <div className="w-full h-[2px] md:my-8 my-4 bg-black dark:bg-white" />
-            <h2>©2023 Ondřej Všetička</h2>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/components/Work/Projects.tsx
+++ src/components/Work/Projects.tsx
@@ -1,32 +1,33 @@
-import { motion } from "framer-motion";
-import Kemp from "./Kemp";
+import { motion } from 'framer-motion';
+import Kemp from './Kemp';
 
 const Projects = () => {
-  return (
-    <>
-      <motion.div
-        initial={{ opacity: 0, y: "25%" }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        viewport={{ once: true }}
-        id="Projects"
-        className="container mx-auto px-8  py-[32px]">
-        <div className="mb-12">
-          <div className="flex flex-row items-center md:justify-start justify-center">
-            <div className="bg-zinc-700 dark:bg-white/30 w-4 h-0.5 mr-2" />
-            <h2 className="font-semibold text-xs text-zinc-700 dark:text-white/30">
-              Portfolio
-            </h2>
-            <div className="bg-zinc-700 dark:bg-white/30 w-4 h-0.5 ml-2 md:hidden" />
-          </div>
-          <h2 className="font-bold text-4xl text-center md:text-start">
-            Mé vybrané projekty
-          </h2>
-        </div>
+    return (
+        <>
+            <motion.div
+                initial={{ opacity: 0, y: '25%' }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                viewport={{ once: true }}
+                id="Projects"
+                className="container mx-auto px-8  py-[32px]"
+            >
+                <div className="mb-12">
+                    <div className="flex flex-row items-center md:justify-start justify-center">
+                        <div className="bg-zinc-700 dark:bg-white/30 w-4 h-0.5 mr-2" />
+                        <h2 className="font-semibold text-xs text-zinc-700 dark:text-white/30">
+                            PORTFOLIO
+                        </h2>
+                        <div className="bg-zinc-700 dark:bg-white/30 w-4 h-0.5 ml-2 md:hidden" />
+                    </div>
+                    <h2 className="font-bold text-4xl text-center md:text-start">
+                        Mé vybrané projekty
+                    </h2>
+                </div>
 
-        <Kemp />
-      </motion.div>
-    </>
-  );
+                <Kemp />
+            </motion.div>
+        </>
+    );
 };
 export default Projects;
