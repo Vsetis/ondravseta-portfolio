619f94d5d24986922f6c94eaec36387620ec4a09
Vsetis <126694324+Vsetis@users.noreply.github.com>
2024-01-24 20:52:05 +0100

    update website

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,15 +2,18 @@ import { SiTailwindcss } from 'react-icons/si';
 import { TiCss3 } from 'react-icons/ti';
 import { AiOutlineHtml5 } from 'react-icons/ai';
 import { FaReact, FaVuejs, FaFigma } from 'react-icons/fa';
+import { SiTrpc, SiExpress } from 'react-icons/si';
 import { motion } from 'framer-motion';
 
 const skills = [
     { title: 'React', icon: FaReact, delay: 0 },
     { title: 'Vue', icon: FaVuejs, delay: 0.3 },
-    { title: 'TailwindCSS', icon: SiTailwindcss, delay: 0.6 },
-    { title: 'HTML', icon: AiOutlineHtml5, delay: 0.9 },
-    { title: 'CSS', icon: TiCss3, delay: 1.2 },
-    { title: 'Figma', icon: FaFigma, delay: 1.5 },
+    { title: 'tRPC', icon: SiTrpc, delay: 0.5 },
+    { title: 'Express.js', icon: SiExpress, delay: 0.8 },
+    { title: 'TailwindCSS', icon: SiTailwindcss, delay: 0.9 },
+    { title: 'HTML', icon: AiOutlineHtml5, delay: 1.2 },
+    { title: 'CSS', icon: TiCss3, delay: 1.5 },
+    { title: 'Figma', icon: FaFigma, delay: 1.8 },
 ];
 
 export default function About() {
@@ -30,19 +33,18 @@ export default function About() {
                                 Něco málo o mně
                             </h2>
                         </div>
-                        <div className="text-md text-center md:text-start  text-black/70 dark:text-white/90 mb-4 max-w-[550px]">
+                        <div className="text-md text-center md:text-start  text-black/70 dark:text-white/90 mb-4 max-w-md">
                             <p className="text-justify">
-                                Jmenuji se Ondřej Všetička, je mi 19 let a
-                                vystudoval jsem obor Informační technologie.
-                                Tvorba webových stránek a aplikací je moje
-                                vášeň, nesmírně mě to baví, a proto neustále
-                                sleduji nové trendy a zlepšuji se.
+                                Jmenuji se Ondřej Všetička, je mi 19 let. Jsem
+                                velký fanoušek technologií a každý den se učím
+                                novým věcem. Věnuji se převážně vývoji, ale také
+                                designu.
                             </p>
                             <p className="mt-2 text-justify">
-                                Nemám rád zastaralé věci, a proto weby tvořím
-                                pomocí nejmodernějších technologií. Osobně si
-                                myslím, že v každém oboru bychom měli jít s
-                                dobou.
+                                Nemám rád zastaralé technologie, a proto weby
+                                tvořím pomocí nejmodernějších technologií.
+                                Osobně si myslím, že v každém oboru bychom měli
+                                jít s dobou.
                             </p>
                         </div>
                     </div>
@@ -51,7 +53,7 @@ export default function About() {
                             Mé dovednosti
                         </h2>
                         <div className="md:w-[25%] w-[75%] h-[2px] bg-black dark:bg-white/90 mb-8" />
-                        <div className="flex flex-wrap text-black/70 dark:text-white/80 items-center gap-6 xl:gap-12">
+                        <div className="grid grid-cols-4 text-black/70 dark:text-white/80 gap-6">
                             {skills.map((skill, index) => (
                                 <motion.div
                                     initial={{ opacity: 0, y: '20%' }}
